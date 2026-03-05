7b824db11cd0617a404172102664fe9528151e0c
Technology iGen <igentech1@gmail.com>
2026-03-05 08:44:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9509206860-40e8b",
+  "appId": "1:88538879711:web:1af6448b6a1cfad0913b6a",
+  "apiKey": "AIzaSyD9EZ7otTLwZCeA_SztV2bECkzgIIGE018",
+  "authDomain": "studio-9509206860-40e8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88538879711"
+};
