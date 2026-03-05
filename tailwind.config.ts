889b4c787d86e5244448d4206329102cce6ad191
Technology iGen <igentech1@gmail.com>
2026-03-05 08:39:19 +0000

    change font of text in this section to Toyota Type (_for element div_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
+        toyota: ['"Toyota Type"', 'Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
